69333cdce5a9933023aa964221f26ba983b80622
alicesharma201 <alicesharma201@gmail.com>
2026-01-26 21:44:58 +0530

    Today I solved gcd

--- Step1/Lec4/4_gcd.cpp
+++ Step1/Lec4/4_gcd.cpp
@@ -1,35 +1,30 @@
-#include <bits/stdc++.h>
-
+#include<bits/stdc++.h>
 int brute(int n1, int n2){
     int gcd = 1;
     for(int i = 1; i <= min(n1, n2); i++){
-        if(n1 % i == 0 && n2 % i == 0){
-            gcd = i;
-        }
+        if(n1 % i == 0 && n2 % i == 0) gcd = i;
     }
     return gcd;
 }
-
 int better(int n1, int n2){
-    int gcd = 1;
-    for(int i = min(n1, n2); i > 0; i--){
+    int gcd;
+    for(int i = min(n1, n2); i >= 1; i--){
         if(n1 % i == 0 && n2 % i == 0){
             return i;
-        }
+            // we will return i as soon as the conditional statement is true for the first time, if not then it'll simply return 1
+        }  
     }
     return 1;
 }
-
-int optimal(int n1, int n2){ // euclidean algorithm
-    if(n1 == min(n1,n2)){
-        do{
-
-        }while(n2 < n2-n1);
-        if(n2 > n2-n1) 
+int optimal(int n1, int n2){
+    // Euclidean Algo
+    while(n1 > 0 && n2 > 0){
+        if(n1 > n2) n1 = n1 % n2;
+        else n2 = n2 % n1;
     }
-
+    if(n1 == 0) return n2;
+    return n1;
 }
-
 int main(){
     int n1, n2;
     cin >> n1 >> n2;
--- practice.cpp
+++ practice.cpp
@@ -1,30 +1,35 @@
 #include<bits/stdc++.h>
-// 123
-// num = 3
-// n = 12
-// reverse = 3
-
-// num = 2
-// n = 1
-// reverse = 32
-
-// num = 1
-// n = 0
-// reverse = 321
-
-int revnum(int n){
-    int num = 0, reverse = 0;
-    while(n > 0){
-        num = n % 10;
-        n = n / 10;
-        reverse = (reverse*10)+num;
+int brute(int n1, int n2){
+    int gcd = 1;
+    for(int i = 1; i <= min(n1, n2); i++){
+        if(n1 % i == 0 && n2 % i == 0) gcd = i;
     }
-    return reverse;
+    return gcd;
 }
-
+int better(int n1, int n2){
+    int gcd;
+    for(int i = min(n1, n2); i >= 1; i--){
+        if(n1 % i == 0 && n2 % i == 0){
+            return i;
+            // we will return i as soon as the conditional statement is true for the first time, if not then it'll simply return 1
+        }  
+    }
+    return 1;
+}
+int optimal(int n1, int n2){
+    // Euclidean Algo
+    while(n1 > 0 && n2 > 0){
+        if(n1 > n2) n1 = n1 % n2;
+        else n2 = n2 % n1;
+    }
+    if(n1 == 0) return n2;
+    return n1;
+}
 int main(){
-    int n;
-    cin >> n;
-    cout << revnum(n);
+    int n1, n2;
+    cin >> n1 >> n2;
+    cout << brute(n1, n2) << endl;
+    cout << better(n1, n2) << endl;
+    cout << optimal(n1, n2) << endl;
     return 0;
 }
