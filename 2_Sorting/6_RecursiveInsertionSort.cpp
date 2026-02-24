806439e1334ddadc6fe670a157517051c2aa9c78
alicesharma201 <alicesharma201@gmail.com>
2026-02-24 20:38:10 +0530

    Today I learned recursive insertion sort

--- 2_Sorting/6_RecursiveInsertionSort.cpp
+++ 2_Sorting/6_RecursiveInsertionSort.cpp
@@ -1,19 +1,20 @@
 #include<bits/stdc++.h>
-void insertion(vector<int>& vec, int n){
-    for(int i = 1; i < n; i++){
-        int key = vec[i];
-        int j = i - 1;
-        while(j >= 0 && vec[j] > key){
-            vec[j+1] = vec[j];
-            j--;
-        }
-        vec[j+1] = key;
+void recInsertion(vector<int>& vec, int n, int i){
+    if(n == i) return;
+    int key = vec[i];
+    int j = i-1;
+    while(j >= 0 && vec[j] > key){
+        vec[j+1] = vec[j];
+        j--;
     }
+    vec[j+1] = key;
+    recInsertion(vec, n, i+1);
 }
 int main(){
     vector<int> vec = {3, 6, 2, 7, 4, 1, 5};
     int n = vec.size();
-    insertion(vec, n);
+    // insertion(vec, n);
+    recInsertion(vec, n, 1);
     for(auto it : vec) cout << it << ' ';
     return 0;
 }
