9a70dd6a1d403964a0da20fe13b8fddea6863bb0
alicesharma201 <alicesharma201@gmail.com>
2026-03-02 23:48:41 +0530

    found optimal solution for removing duplicates in an array

--- 3_Arrays/Easy/4_RemoveDuplicates.cpp
+++ 3_Arrays/Easy/4_RemoveDuplicates.cpp
@@ -1,17 +1,17 @@
+/*
+Problem Statement: 
+Given an integer array sorted in non-decreasing order, remove the duplicates "in place" such that each unique element appears only once. The relative order of the elements should be kept the same.
+If there are k elements after removing the duplicates, then the first k elements of the array should hold the final result. It does not matter what you leave beyond the first k elements.
+
+“In-place” means:
+You must modify the original data structure itself
+without using extra memory that grows with input size.
+Extra Space Complexity should be O(1)
+*/
 #include<bits/stdc++.h>
-vector<int> self(vector<int>& vec){
-    int n = vec.size();
-    set<int> temp;
-    for(int i = 0; i < n; i++){
-        temp.insert(vec[i]);
-    }
-    vec.clear();
-    for(int item : temp){
-        vec.emplace_back(item);
-    }
-    return vec;
-}
 int brute(vector<int>& vec){
+    // TC: O(n)
+    // SC: O(n)
     unordered_set<int> seen;
     int index = 0;
     for(int num : vec){
@@ -23,13 +23,32 @@ int brute(vector<int>& vec){
     }
     return index;
 }
+int optimal(vector<int>& vec){
+    // TC: O(n)
+    // SC: O(1)
+    int i = 0;
+    int n = vec.size();
+    for(int j = 1; j < n; j++){
+        if(vec[i] != vec[j]){
+            vec[i+1] = vec[j];
+            i++;
+        }
+    }
+    return i+1; // here i is the 'index' till the unique elements are present
+}
 int main(){
-    vector<int> vec = {1,1,2,2,2,3,3,4};
-    // self(vec);
-    int k = brute(vec);
+    vector<int> vec1 = {1,1,2,2,2,3,3,4};
+    int k = brute(vec1);
     cout << "k = " << k << "\nArray after removing duplicates: ";
     for(int i = 0; i < k; i++){
-        cout << vec[i] << " ";
+        cout << vec1[i] << " ";
+    }
+    cout << endl;
+    vector<int> vec2 = {1,1,2,2,2,3,3};
+    int K = optimal(vec2);
+    cout << "k = " << K << "\nArray after removing duplicates: ";
+    for(int i = 0; i < K; i++){
+        cout << vec2[i] << " ";
     }
     return 0;
 }
