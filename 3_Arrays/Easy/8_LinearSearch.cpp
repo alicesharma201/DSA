91653924e09072c4a364e73e05490fabd274c4e7
alicesharma201 <alicesharma201@gmail.com>
2026-03-05 04:32:36 +0530

    Linear Search

---
+++ 3_Arrays/Easy/8_LinearSearch.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+int linearSearch(int arr[], int n, int find){
+    for(int i = 0; i < n; i++){
+        if(arr[i] == find) return i;
+    }
+    return -1;
+}
+int main(){
+    int arr[] = {3, 2, 1, 6, 7, 8};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    int find = 5;
+    int result = linearSearch(arr, n, find);
+    if(result == -1) cout << "Element not found";
+    else cout << "Element " << find << " is found at index " << result;
+    return 0;
+}
