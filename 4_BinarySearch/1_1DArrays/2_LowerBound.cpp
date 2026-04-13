7c3939afbe60b6284f4e9b89decd64e1f50c874d
alicesharma201 <alicesharma201@gmail.com>
2026-04-13 21:05:03 +0530

    Lower Bound

---
+++ 4_BinarySearch/1_1DArrays/2_LowerBound.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+int brute(vector<int>& nums, int x){
+    // TC: O(n)
+    // SC: O(1)
+    int n = nums.size();
+    for(int i = 0; i < n; i++){
+        if(nums[i] >= x) return i;
+    }
+    return n;
+}
+int optimal(vector<int>& nums, int x){
+    // TC: O(logn)
+    // SC: O(1)
+    int n = nums.size();
+    int ans = n;
+    int low = 0, high = n-1;
+    while(low <= high){
+        int mid = (low + high)/2;
+        if(nums[mid] >= x){
+            ans = mid;
+            high = mid-1;
+        }
+        else{
+            low = mid+1;
+        }
+    }
+    return ans;
+}
+int main(){
+    vector<int> nums = {3, 5, 8, 15, 19};
+    int x = 9;
+    cout << brute(nums, x) << endl; // we are inserting x at this position
+    cout << optimal(nums, x) << endl;
+    return 0;
+}
