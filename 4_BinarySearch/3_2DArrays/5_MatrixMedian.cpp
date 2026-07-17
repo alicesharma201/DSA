0614c9fcb79978a3ea43d8b7d6b2526cf42d3ae5
alicesharma201 <alicesharma201@gmail.com>
2026-07-17 23:38:49 +0530

    Refine approach and add comments

--- 4_BinarySearch/3_2DArrays/5_MatrixMedian.cpp
+++ 4_BinarySearch/3_2DArrays/5_MatrixMedian.cpp
@@ -12,30 +12,33 @@ int bruteMedian(vector<vector<int>>& mat){
     sort(temp.begin(), temp.end());
     return temp[(m*n)/2];
 }
-int countLessEqual(vector<int>& row, int mid){
-    return upper_bound(row.begin(), row.end(), mid) - row.begin();
-}
-int optimalMedian(vector<vector<int>>& matrix){
-    // TC: O(log(maxValue−minValue)×rows×log(cols))
-    // SC: O(1)
-    int rows = matrix.size();
-    int cols = matrix[0].size();
-    int low = matrix[0][0];
-    int high = matrix[0][cols - 1];
-    for (int i = 1; i < rows; i++) {
-        low = min(low, matrix[i][0]);
-        high = max(high, matrix[i][cols - 1]);
+int countSmallerEquals(vector<vector<int>>& mat, int x){
+    int cnt = 0;
+    for(int i = 0; i < mat.size(); i++){
+        cnt += upper_bound(mat[i].begin(), mat[i].end(), x) - mat[i].begin();
     }
-    while (low < high) {
-        int mid = (low + high) / 2;
-        int count = 0;
-        for (int i = 0; i < rows; i++) {
-            count += countLessEqual(matrix[i], mid);
-        }
-        if (count < (rows * cols + 1) / 2)
-            low = mid + 1;
-        else
-            high = mid;
+    return cnt;
+}
+int optimalMedian(vector<vector<int>>& mat){
+    // TC: O(log(maxValue-minValue) * rows * log(col)) 
+    // SC: O(1)
+    
+    int r = mat.size(), c = mat[0].size();
+    int low = mat[0][0], high = mat[0][c-1];
+
+    // the median must be bigger than (r*c)/2 elements
+    // [no. of elements <= median] > (r*c)/2 
+    int required = (r*c)/2;
+
+    for(int i = 0; i < r; i++){
+        low = min(low, mat[i][0]);
+        high = max(high, mat[i][c-1]);
+    }
+    while(low <= high){
+        int mid = low + (high - low)/2;
+        int smallerEquals = countSmallerEquals(mat, mid);
+        if(smallerEquals <= required) low = mid + 1;
+        else high = mid - 1;
     }
     return low;
 }
