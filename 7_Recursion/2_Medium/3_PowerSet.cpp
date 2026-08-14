250fc96294aa35057ee7f6eed1624fe27296ef50
alicesharma201 <alicesharma201@gmail.com>
2026-08-14 16:56:57 +0530

    Power Set (recursion)

--- 7_Recursion/2_Medium/3_PowerSet.cpp
+++ 7_Recursion/2_Medium/3_PowerSet.cpp
@@ -17,10 +17,30 @@ vector<string> powerSet1(string str){
     }
     return substrings;
 }
+void includeExcludeDecision(string& str, string& val, int i, vector<string>& substrings){
+    if(i == str.size()){
+        substrings.push_back(val);
+        return;
+    }
+    val.push_back(str[i]);
+    includeExcludeDecision(str, val, i+1, substrings); // include
+    val.pop_back();
+    includeExcludeDecision(str, val, i+1, substrings); // exclude
+}
+vector<string> powerSet2(string str){
+    // TC: O(n * 2^n)
+    // SC: O(n * 2^n)
+    int n = str.size();
+    vector<string> substrings;
+    string val;
+    includeExcludeDecision(str, val, 0, substrings);
+    return substrings;
+}
 int main(){
     string str = "abc";
     // string str = "aa";
-    vector<string> res = powerSet1(str); // bit manipulations
+    // vector<string> res = powerSet1(str); // bit manipulations
+    vector<string> res = powerSet2(str); // recursion
     for(string i : res){
         cout << "\"" << i << "\"" << " ";
     }
