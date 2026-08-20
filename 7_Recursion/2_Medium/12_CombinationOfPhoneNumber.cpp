d17896a61d0a88286c229e55c50d8ab55e892e56
alicesharma201 <alicesharma201@gmail.com>
2026-08-20 19:56:15 +0530

    Letter Combinations of a Phone Number

---
+++ 7_Recursion/2_Medium/12_CombinationOfPhoneNumber.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+void helper(int i, string digits, vector<string>& letters, vector<string>& res, string val){
+    if(i == digits.size()){
+        res.push_back(val);
+        return;
+    }
+    for(int x = 0; x < letters[digits[i]-'2'].size(); x++){
+        helper(i+1, digits, letters, res, val+letters[digits[i]-'2'][x]);
+    }
+}
+vector<string> letterCombinations(string digits){
+    // TC: O(n * 4^n)
+    // SC: O(n * 4^n)
+    if(digits.empty()) return {};
+    vector<string> letters = {"abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+    vector<string> res;
+    string val;
+    helper(0, digits, letters, res, val);
+    return res;
+}
+int main(){
+    
+    return 0;
+}
