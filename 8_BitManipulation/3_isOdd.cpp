90623a3e3db4692990b6c176c220a01aca57c871
alicesharma201 <alicesharma201@gmail.com>
2026-08-27 18:33:21 +0530

    Is Odd

---
+++ 8_BitManipulation/3_isOdd.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+bool isOdd(int n){
+    return (n % 2 == 1); // return true if odd
+}
+int main(){
+    cout << boolalpha << isOdd(7);
+    return 0;
+}
