b61a193f61574cfc16e9c64d113e9f6ff65a0334
alicesharma201 <alicesharma201@gmail.com>
2026-08-27 18:47:19 +0530

    Power of 2

---
+++ 8_BitManipulation/4_PowerOf2.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+bool isPowerOfTwo(int n) {
+    if(n <= 0) return false;
+    return ((n & (n-1)) == 0);
+}
+int main(){
+    cout << boolalpha << isPowerOfTwo(16);
+    return 0;
+}
