6cff9d081fa4ed346855bfb0b02e6edce1d17fbf
alicesharma201 <alicesharma201@gmail.com>
2026-08-28 10:08:37 +0530

    Set Rightmost Unset Bit

---
+++ 8_BitManipulation/6_SetRightmostUnsetBit.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int setRightmostUnsetBit(int n){
+    return n | n+1;
+}
+int main(){
+    int n = 10;
+    cout << setRightmostUnsetBit(n);
+    return 0;
+}
